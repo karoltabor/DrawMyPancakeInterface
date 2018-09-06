83cf99938a7ec12c6577c2f7b4fa0e824f1ba179
Karol Tabor <karl_2@live.nl>
2018-09-06 23:57:04 +0200

    Added Logo to Resources. Added a TCP/IP connect button to test EV3 connectivity

--- DrawMyPancakeInterface/DrawMyPancakeInterface/Form1.Designer.cs
+++ DrawMyPancakeInterface/DrawMyPancakeInterface/Form1.Designer.cs
@@ -28,37 +28,120 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnConnectTest = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tbAdress = new System.Windows.Forms.TextBox();
+            this.tbPort = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnConnectStop = new System.Windows.Forms.Button();
+            this.tbStatus = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
+            // btnConnectTest
+            // 
+            this.btnConnectTest.Location = new System.Drawing.Point(686, 65);
+            this.btnConnectTest.Name = "btnConnectTest";
+            this.btnConnectTest.Size = new System.Drawing.Size(99, 23);
+            this.btnConnectTest.TabIndex = 1;
+            this.btnConnectTest.Text = "connect Test";
+            this.btnConnectTest.UseVisualStyleBackColor = true;
+            this.btnConnectTest.Click += new System.EventHandler(this.btnConnectTest_Click);
+            // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(-147, -101);
+            this.pictureBox1.BackgroundImage = global::DrawMyPancakeInterface.Properties.Resources.WhatsApp_Image_2018_09_06_at_15_10_44;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(776, 443);
-            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.Size = new System.Drawing.Size(330, 217);
+            this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
+            // 
+            // tbAdress
+            // 
+            this.tbAdress.Location = new System.Drawing.Point(686, 13);
+            this.tbAdress.Name = "tbAdress";
+            this.tbAdress.Size = new System.Drawing.Size(100, 20);
+            this.tbAdress.TabIndex = 3;
+            this.tbAdress.Text = "194.126.0.1";
+            // 
+            // tbPort
+            // 
+            this.tbPort.Location = new System.Drawing.Point(686, 39);
+            this.tbPort.Name = "tbPort";
+            this.tbPort.Size = new System.Drawing.Size(44, 20);
+            this.tbPort.TabIndex = 4;
+            this.tbPort.Text = "8080";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(631, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Host ip";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(645, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Port";
+            // 
+            // btnConnectStop
+            // 
+            this.btnConnectStop.Location = new System.Drawing.Point(686, 95);
+            this.btnConnectStop.Name = "btnConnectStop";
+            this.btnConnectStop.Size = new System.Drawing.Size(100, 23);
+            this.btnConnectStop.TabIndex = 7;
+            this.btnConnectStop.Text = "Stop connection";
+            this.btnConnectStop.UseVisualStyleBackColor = true;
+            // 
+            // tbStatus
+            // 
+            this.tbStatus.Location = new System.Drawing.Point(686, 125);
+            this.tbStatus.Multiline = true;
+            this.tbStatus.Name = "tbStatus";
+            this.tbStatus.Size = new System.Drawing.Size(220, 45);
+            this.tbStatus.TabIndex = 9;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1837, 984);
+            this.ClientSize = new System.Drawing.Size(918, 512);
+            this.Controls.Add(this.tbStatus);
+            this.Controls.Add(this.btnConnectStop);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbPort);
+            this.Controls.Add(this.tbAdress);
             this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnConnectTest);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
+        private System.Windows.Forms.Button btnConnectTest;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox tbAdress;
+        private System.Windows.Forms.TextBox tbPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnConnectStop;
+        private System.Windows.Forms.TextBox tbStatus;
     }
 }
 
