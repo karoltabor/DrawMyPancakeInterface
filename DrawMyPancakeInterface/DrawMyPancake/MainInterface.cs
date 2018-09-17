8a291c3258e0df90256e224e60b28b1594007cbb
417211 <417211@DESKTOP-V3R7Q6M>
2018-09-17 10:12:13 +0200

    New Interface design

---
+++ DrawMyPancakeInterface/DrawMyPancake/MainInterface.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PanelTesting
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ DrawMyPancakeInterface/DrawMyPancakeInterface/MainInterface.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace DrawMyPancakeInterface
+{
+    partial class MainInterface
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainInterface));
+            this.rightPanel = new System.Windows.Forms.Panel();
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnUndo = new System.Windows.Forms.Button();
+            this.btnPencil = new System.Windows.Forms.Button();
+            this.mainTableLayout = new System.Windows.Forms.TableLayoutPanel();
+            this.leftPanel = new System.Windows.Forms.Panel();
+            this.btnTemp = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.centerTableLayout = new System.Windows.Forms.TableLayoutPanel();
+            this.rightPanel.SuspendLayout();
+            this.mainTableLayout.SuspendLayout();
+            this.leftPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rightPanel
+            // 
+            this.rightPanel.Controls.Add(this.btnDraw);
+            this.rightPanel.Controls.Add(this.btnClear);
+            this.rightPanel.Controls.Add(this.btnUndo);
+            this.rightPanel.Controls.Add(this.btnPencil);
+            this.rightPanel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.rightPanel.Location = new System.Drawing.Point(680, 3);
+            this.rightPanel.Name = "rightPanel";
+            this.rightPanel.Size = new System.Drawing.Size(117, 444);
+            this.rightPanel.TabIndex = 1;
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(154)))), ((int)(((byte)(66)))));
+            this.btnDraw.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnDraw.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.btnDraw.FlatAppearance.BorderSize = 0;
+            this.btnDraw.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDraw.Image = ((System.Drawing.Image)(resources.GetObject("btnDraw.Image")));
+            this.btnDraw.Location = new System.Drawing.Point(0, 300);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnDraw.Size = new System.Drawing.Size(117, 100);
+            this.btnDraw.TabIndex = 3;
+            this.btnDraw.UseVisualStyleBackColor = false;
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(83)))), ((int)(((byte)(42)))));
+            this.btnClear.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnClear.FlatAppearance.BorderSize = 0;
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClear.Image = ((System.Drawing.Image)(resources.GetObject("btnClear.Image")));
+            this.btnClear.Location = new System.Drawing.Point(0, 200);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnClear.Size = new System.Drawing.Size(117, 100);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.UseVisualStyleBackColor = false;
+            // 
+            // btnUndo
+            // 
+            this.btnUndo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(116)))), ((int)(((byte)(185)))));
+            this.btnUndo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnUndo.FlatAppearance.BorderSize = 0;
+            this.btnUndo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUndo.Image = ((System.Drawing.Image)(resources.GetObject("btnUndo.Image")));
+            this.btnUndo.Location = new System.Drawing.Point(0, 100);
+            this.btnUndo.Name = "btnUndo";
+            this.btnUndo.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnUndo.Size = new System.Drawing.Size(117, 100);
+            this.btnUndo.TabIndex = 1;
+            this.btnUndo.UseVisualStyleBackColor = false;
+            // 
+            // btnPencil
+            // 
+            this.btnPencil.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(171)))), ((int)(((byte)(225)))));
+            this.btnPencil.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnPencil.FlatAppearance.BorderSize = 0;
+            this.btnPencil.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPencil.Image = ((System.Drawing.Image)(resources.GetObject("btnPencil.Image")));
+            this.btnPencil.Location = new System.Drawing.Point(0, 0);
+            this.btnPencil.Name = "btnPencil";
+            this.btnPencil.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnPencil.Size = new System.Drawing.Size(117, 100);
+            this.btnPencil.TabIndex = 0;
+            this.btnPencil.UseVisualStyleBackColor = false;
+            // 
+            // mainTableLayout
+            // 
+            this.mainTableLayout.ColumnCount = 3;
+            this.mainTableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 21.56573F));
+            this.mainTableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 78.43427F));
+            this.mainTableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 122F));
+            this.mainTableLayout.Controls.Add(this.rightPanel, 2, 0);
+            this.mainTableLayout.Controls.Add(this.leftPanel, 0, 0);
+            this.mainTableLayout.Controls.Add(this.centerTableLayout, 1, 0);
+            this.mainTableLayout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainTableLayout.Location = new System.Drawing.Point(0, 0);
+            this.mainTableLayout.Name = "mainTableLayout";
+            this.mainTableLayout.RowCount = 1;
+            this.mainTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.mainTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.mainTableLayout.Size = new System.Drawing.Size(800, 450);
+            this.mainTableLayout.TabIndex = 5;
+            // 
+            // leftPanel
+            // 
+            this.leftPanel.AutoScroll = true;
+            this.leftPanel.Controls.Add(this.button4);
+            this.leftPanel.Controls.Add(this.button3);
+            this.leftPanel.Controls.Add(this.button2);
+            this.leftPanel.Controls.Add(this.button1);
+            this.leftPanel.Controls.Add(this.btnTemp);
+            this.leftPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.leftPanel.Location = new System.Drawing.Point(3, 3);
+            this.leftPanel.Name = "leftPanel";
+            this.leftPanel.Size = new System.Drawing.Size(140, 444);
+            this.leftPanel.TabIndex = 2;
+            // 
+            // btnTemp
+            // 
+            this.btnTemp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(171)))), ((int)(((byte)(225)))));
+            this.btnTemp.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnTemp.FlatAppearance.BorderSize = 0;
+            this.btnTemp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTemp.Image = ((System.Drawing.Image)(resources.GetObject("btnTemp.Image")));
+            this.btnTemp.Location = new System.Drawing.Point(0, 0);
+            this.btnTemp.Name = "btnTemp";
+            this.btnTemp.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnTemp.Size = new System.Drawing.Size(123, 100);
+            this.btnTemp.TabIndex = 3;
+            this.btnTemp.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(171)))), ((int)(((byte)(225)))));
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(0, 100);
+            this.button1.Name = "button1";
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button1.Size = new System.Drawing.Size(123, 100);
+            this.button1.TabIndex = 4;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(171)))), ((int)(((byte)(225)))));
+            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(0, 200);
+            this.button2.Name = "button2";
+            this.button2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button2.Size = new System.Drawing.Size(123, 100);
+            this.button2.TabIndex = 5;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(171)))), ((int)(((byte)(225)))));
+            this.button3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.Location = new System.Drawing.Point(0, 300);
+            this.button3.Name = "button3";
+            this.button3.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button3.Size = new System.Drawing.Size(123, 100);
+            this.button3.TabIndex = 6;
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(171)))), ((int)(((byte)(225)))));
+            this.button4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
+            this.button4.Location = new System.Drawing.Point(0, 400);
+            this.button4.Name = "button4";
+            this.button4.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button4.Size = new System.Drawing.Size(123, 100);
+            this.button4.TabIndex = 7;
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // centerTableLayout
+            // 
+            this.centerTableLayout.ColumnCount = 1;
+            this.centerTableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.centerTableLayout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.centerTableLayout.Location = new System.Drawing.Point(149, 3);
+            this.centerTableLayout.Name = "centerTableLayout";
+            this.centerTableLayout.RowCount = 2;
+            this.centerTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.centerTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 344F));
+            this.centerTableLayout.Size = new System.Drawing.Size(525, 444);
+            this.centerTableLayout.TabIndex = 3;
+            // 
+            // MainInterface
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(51)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mainTableLayout);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainInterface";
+            this.Text = "Draw My Pancake";
+            this.rightPanel.ResumeLayout(false);
+            this.mainTableLayout.ResumeLayout(false);
+            this.leftPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel rightPanel;
+        private System.Windows.Forms.Button btnPencil;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnUndo;
+        private System.Windows.Forms.Button btnDraw;
+        private System.Windows.Forms.TableLayoutPanel mainTableLayout;
+        private System.Windows.Forms.Panel leftPanel;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnTemp;
+        private System.Windows.Forms.TableLayoutPanel centerTableLayout;
+    }
+}
